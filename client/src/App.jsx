951f7ac30d4dbeeed59b4d855899bc8b87fa1e72
GudinoJoaquin <JoaGudi@hotmail.com>
2024-05-18 19:02:05 -0300

    sdsdf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import UserAnuncios from "./pages/@user/UserAnuncios";
 import AdminAnuncios from "./pages/@admin/AdminAnuncios";
 import CrearAnuncio from "./pages/@admin/CrearAnuncio";
 import ModificarAnuncio from "./pages/@admin/ModificarAnuncios";
 import Login from "./pages/@admin/Login";
-import checkUser from "./assets/js/checkUser.js";
 import Nav from "./components/Nav.jsx";
 import Footer from "./components/Footer.jsx";
 import NotFound from "./pages/NotFound.jsx";
@@ -12,7 +12,6 @@ import NotFound from "./pages/NotFound.jsx";
 // import Nosotros from "./pages/home/Nosotros.jsx";
 
 export default function App() {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
   const [userType, setUserType] = useState("user");
 
   useEffect(() => {
@@ -27,32 +26,29 @@ export default function App() {
     }
   }, []);
 
-  const handleNavigation = (path) => {
-    setCurrentPath(path);
-    window.location.href = path;
-  };
-
   return (
-    <>
-      {userType !== "admin" ? (
-        <>
-          <Nav />
-          {currentPath === "/" && <UserAnuncios />}
-          {currentPath === "/login" && <Login />}
-          {currentPath === "/home" && <Home />}
-          {currentPath === '/nosotros' && <Nosotros/>}
-          <Footer />
-        </>
-      ) : (
-        <>
-          <Nav />
-          {currentPath === "/" && <AdminAnuncios />}
-          {currentPath === "/login" && <Login />}
-          {currentPath === "/admin/crearAnuncio" && <CrearAnuncio />}
-          {currentPath === "/admin/editarAnuncio" && <ModificarAnuncio />}
-          <Footer />
-        </>
-      )}
-    </>
+    <Router>
+      <Nav />
+      <Routes>
+        {userType !== "admin" ? (
+          <>
+            <Route path="/" element={<UserAnuncios />} />
+            <Route path="/login" element={<Login />} />
+            {/* Asegúrate de descomentar estas líneas si Home y Nosotros están definidos */}
+            {/* <Route path="/home" element={<Home />} /> */}
+            {/* <Route path="/nosotros" element={<Nosotros />} /> */}
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<AdminAnuncios />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin/crearAnuncio" element={<CrearAnuncio />} />
+            <Route path="/admin/editarAnuncio" element={<ModificarAnuncio />} />
+          </>
+        )}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
