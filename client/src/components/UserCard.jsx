4b981723303edfbfb1588281f4b6a37bd9e34657
GudinoJoaquin <JoaGudi@hotmail.com>
2024-05-19 19:54:57 -0300

    dafsdf

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -31,21 +31,27 @@ export default function UserCard({ img, titulo, mensaje, fecha, adjunto }) {
   };
 
   return (
-    <div className="flex justify-center items-center mt-[100px] overflow-x-hidden bg-white dark:bg-slate-900">
-      <div className="relative w-full max-w-md">
+    <div className="flex justify-center items-center mt-[100px] overflow-x-hidden bg-white dark:bg-slate-900 p-4 sm:p-8 scroll-smooth">
+      <div className="relative w-full max-w-lg lg:max-w-xl xl:max-w-2xl">
         <div className="overflow-hidden rounded-md">
-          <img className="w-full h-auto object-cover filter brightness-50" src={img} alt={titulo} />
+          <img
+            className="w-full h-auto object-cover filter brightness-50"
+            src={img}
+            alt={titulo}
+          />
           <div className="absolute top-0 left-0 w-full h-full">
             <div className="absolute top-4 left-4 bg-white bg-opacity-20 backdrop-blur-lg p-2 rounded-md">
               <h5 className="text-white text-lg font-bold">{titulo}</h5>
             </div>
             <div className="absolute top-4 right-4 bg-white bg-opacity-20 backdrop-blur-lg p-2 rounded-md">
-              <h5 className="text-white text-sm">{fecha}</h5>
+              <h5 className="text-white text-lg font-bold">{fecha}</h5>
             </div>
           </div>
         </div>
         <div className="mt-4 p-4 bg-white dark:bg-slate-800 rounded-md shadow-md">
-          <p className="text-black dark:text-white">{truncateText(mensaje, 700)}</p>
+          <p className="text-black dark:text-white">
+            {truncateText(mensaje, 700)}
+          </p>
           <button
             data-ripple-light="true"
             type="button"
