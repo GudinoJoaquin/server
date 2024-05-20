76a116236b75b81e3febfa07a67798686d7769d0
GudinoJoaquin <JoaGudi@hotmail.com>
2024-05-20 00:39:28 -0300

    dfdsf

--- server/checkUser.js
+++ server/checkUser.js
@@ -1,5 +1,5 @@
 export default function checkUser(user, pass, ip) {
-  if (user == "admin" && pass == "admin") {
+  if (user === "admin" && pass === "admin") {
     return "admin";
   } else {
     return "user";
