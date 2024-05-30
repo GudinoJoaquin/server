d2e864e6d80d9c3b08445cca95cb7f52801b9cdd
GudinoJoaquin <JoaGudi@hotmail.com>
2024-05-30 16:20:40 -0300

    fhdj

--- CONST.js
+++ CONST.js
@@ -1,2 +1,2 @@
-// export const HOME = 'https://admin-eta-opal.vercel.app'
-export const HOME = 'http://localhost:5173/'
+export const HOME = 'https://admin-eta-opal.vercel.app'
+// export const HOME = 'http://localhost:5173/'
