dd9f6034c288beec83dc9c5ee415bffb19475e12
FacundoCien <130766359+FacundoCien@users.noreply.github.com>
2024-06-05 17:15:04 -0400

    tt

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ import {
   editarAnuncio,
   obtenerAnuncios,
   eliminarAnuncio,
+  verificarUsuario,
 } from "../js/anuncios.js";
 import { enviarCorreo } from "../js/email.js";
 import { checkApiKey } from "../js/verificacion.js";
@@ -15,14 +16,15 @@ router.get("/anuncios/", obtenerAnuncios);
 router.post("/admin/enviar-anuncio", enviarAnuncio);
 router.post("/admin/editar-anuncio", editarAnuncio);
 router.delete("/eliminar-anuncio", eliminarAnuncio);
+router.get("/verificarUsuario" )
 router.get(
   "/resend",
-  // checkApiKey("CBuW$66aWU!MbZ41h^JH^nLAw%^^sh%JfJmp82#ud*YX91Fx5N6%t6%!udFF"),
+  checkApiKey("CBuW$66aWU!MbZ41h^JH^nLAw%^^sh%JfJmp82#ud*YX91Fx5N6%t6%!udFF"),
   enviarCorreo
 );
 router.get(
   "/verificarUsuario",
-  // checkApiKey("CBuW$66aWU!MbZ41h^JH^nLAw%^^sh%JfJmp82#ud*YX91Fx5N6%t6%!udFF"),
+  checkApiKey("CBuW$66aWU!MbZ41h^JH^nLAw%^^sh%JfJmp82#ud*YX91Fx5N6%t6%!udFF"),
   verificarUsuario
 );
 router.post("/updateUsuario", updateUsuario);
