ea009421851e301e33ca36393f977ee241cbfa17
GudinoJoaquin <JoaGudi@hotmail.com>
2024-06-13 12:20:13 -0300

    llñ

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import routes from "./config/routes.js";
+import conexion from "./config/db.js";
 
 const app = express();
 const port = 1234;
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.set("trust proxy", true);
 
 app.use(routes);
+app.use(conexion)
 
 app.listen(port, () => {
   console.log(`Server listening on port http://localhost:${port}`);
