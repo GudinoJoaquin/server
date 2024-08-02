42e24fbff81d3e94eada3656195ba9a74c1a9974
GudinoJoaquin <130807340+GudinoJoaquin@users.noreply.github.com>
2024-08-02 20:25:32 -0300

    Prueba 9

--- config/routes.js
+++ config/routes.js
@@ -15,7 +15,7 @@ router.get("/anuncios", obtenerAnuncios);
 router.post("/admin/enviar-anuncio", enviarAnuncio);
 router.post("/admin/editar-anuncio", editarAnuncio);
 router.delete("/eliminar-anuncio", eliminarAnuncio);
-
+/*
 router.get(
   "/resend",
   checkApiKey("CBuW$66aWU!MbZ41h^JH^nLAw%^^sh%JfJmp82#ud*YX91Fx5N6%t6%!udFF"),
@@ -29,6 +29,6 @@ router.get(
 router.post(
   "/updateUsuario",
   updateUsuario
-);
+); */
 
 export default router;
